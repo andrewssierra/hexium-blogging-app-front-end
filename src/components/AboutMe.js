ff8885aaea8f2a89b8e25c30d265e7999baabf3c
andrewssierra <andrewssierra@yahoo.com>
2020-02-22 23:08:44 -0800

    Adding styling

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import { Avatar } from 'antd';
+import { Avatar, Descriptions, Divider} from 'antd';
 
 class AboutMe extends React.Component {
-
     constructor(props) {
         super(props);
-        this.state = {profilePicture: undefined}
+        this.state = { profilePicture: undefined };
     }
 
-    componentDidMount = async ()=> {
-        this.setState({profilePicture: this.props.me.profilePicture })
-    }
+    componentDidMount = async () => {
+        this.setState({ profilePicture: this.props.me.profilePicture });
+    };
 
-    render(){
+    render() {
         return (
-            <div style={{ margin: '-86px 100px'}}>
+            <div style={{ margin: '-95px 100px' }}>
                 <Avatar
                     size={200}
-                   
                     src={this.state.profilePicture}
                     alt="Profile"
                 />
+                <br />
+                <br />
+                <Divider orientation="left">About Me</Divider>
+                <Descriptions>
+                    <Descriptions.Item label="Name">
+                        {this.props.me.name}
+                    </Descriptions.Item>
+                </Descriptions>
             </div>
         );
     }
-};
+}
 
 export default AboutMe;
